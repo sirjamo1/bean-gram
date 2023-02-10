3c5102fb6302cea15326f15606bb6fbec2ab29e3
Sirjamo1 <jam_how@hotmail.com>
2023-02-10 14:26:04 +1000

    added navbar and created logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import beanGramLogo from "../assets/images/beanGramLogo.png"
+
+
+const Navbar = () => {
+
+ return (<div><img src={beanGramLogo}></img><h1>BEAN</h1></div>)
+}
+
+
+
+export default Navbar
