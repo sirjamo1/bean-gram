5239ae26b8f255e7be0de992df54976a655453f3
Sirjamo1 <jam_how@hotmail.com>
2023-02-12 08:19:59 +1000

    added navbar(header)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import beanGramLogo from "../assets/images/beanGramLogo.png"
+import blankAvatar from "../assets/images/blankAvatar.png"
+
 
 
 const Navbar = () => {
 
- return (<div><img src={beanGramLogo}></img><h1>BEAN</h1></div>)
+ return (
+     <div className="navbar-container">
+         <img src={beanGramLogo} alt="Bean gram logo" width="100px" />
+         <div className="user-info">
+             <h3>BEAN</h3>
+             <img src={blankAvatar}width="50px" />
+         </div>
+     </div>
+ );
 }
 
 
